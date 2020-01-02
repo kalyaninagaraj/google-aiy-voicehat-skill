5d56de0b454c297a32cb672c618221e130bb8f9c
kalyaninagaraj <kalyanin@gmail.com>
2020-01-02 15:19:15 +0000

    testing button logic

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # <img src="https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/cube.svg" card_color="#222222" width="50" height="50" style="vertical-align:bottom"/> Google Aiy Voicehat
-Enables the button and led on the google aiy voicehat
+Enables the button and led on the Google AIY voicehat.
 
 ## About
-This skill uses the `gpiozero` python library to operate the button/led combo on the google aiy voicehat
+This skill follows the code outline of the excellent
+picroft-google-aiy-voicekit skill by andlo, but uses 
+the gpiozero library (instead of RPi.GPIO) to operate
+the button-led combo connected to the voicehat.
 
-## Examples
+The idea is to test gpiozero's ability to handle switch
+bounce when polling for a button press. RPi.GPIO does
+not fare too in these matters. 
+
+If all goes well, I'd like an extended button press 
+(> 7 seconds) to  initiate a system shutdown.
 
 ## Credits
 kalyaninagaraj
@@ -13,7 +21,8 @@ kalyaninagaraj
 **IoT**
 
 ## Tags
-#Google aiy
-#Aiy
-#Google aiy voice
+#Mycroft AI
+#Google
+#AIY
+#voicehat
 
--- __init__.py
+++ __init__.py
@@ -1,15 +1,44 @@
-from mycroft import MycroftSkill, intent_file_handler
+""" This skill follows the code outline of the excellent
+picroft-google-aiy-voicekit skill by @andlo, but uses
+the gpiozero library (instead of RPi.GPIO) to operate
+the button-led combo connected to the voicehat.
 
+The idea is to test gpiozero's ability to handle switch
+bounce when polling for a button press. RPi.GPIO does
+not fare too in these matters.
 
-class GoogleAiyVoicehat(MycroftSkill):
+If all goes well, I'd like an extended button press
+(> 7 seconds) to  initiate a system shutdown.
+"""
+
+#from mycroft.messagebus.message import Message
+from mycroft import MycroftSkill
+from gpiozero import Button, LED
+from signal import pause
+import time
+
+#button = Button(23)
+led = LED(25)
+
+class GoogleAIYVoicehat(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('voicehat.aiy.google.intent')
-    def handle_voicehat_aiy_google(self, message):
-        self.speak_dialog('voicehat.aiy.google')
+    def initialize(self):
+        led.off()
+        self.schedule_repeating_event(self.handle_voicehat_button, None, 0.01, 'buttonpress')
+        #self.add_event('recognizer_loop:record_begin', self.handle_listener_started)
+        #self.add_event('recognizer_loop:record_end', self.handle_listener_ended)
 
+    def handle_voicehat_button(self, message):
+        if button.when_pressed
+            self.say_hello()
+        pause()
+
+    def say_hello(self):
+        led.on()
+        self.speak("Hello!")
+        led.off()
 
 def create_skill():
-    return GoogleAiyVoicehat()
-
+    return GoogleAIYVoicehat()
