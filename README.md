902dfc223ade122c5f4a788f5e5c94bc3c0cbbfa
kalyaninagaraj <kalyanin@gmail.com>
2020-01-03 19:18:51 +0000

    First running version

--- README.md
+++ README.md
@@ -2,17 +2,17 @@
 Enables the button and led on the Google AIY voicehat.
 
 ## About
-This skill follows the code outline of the excellent
-picroft-google-aiy-voicekit skill by andlo, but uses 
-the gpiozero library (instead of RPi.GPIO) to operate
-the button-led combo connected to the voicehat.
+This skill roughly follows the code outline of the 
+excellent picroft-google-aiy-voicekit skill by @andlo, 
+but uses the gpiozero library (instead of RPi.GPIO) to 
+operate the button-led combo connected to the voicehat.
 
-The idea is to test gpiozero's ability to handle switch
-bounce when polling for a button press. RPi.GPIO does
-not fare too in these matters. 
+The idea is to test gpiozero's ability to handle switch 
+bounce when polling for a button press; seems like 
+RPi.GPIO doesn't do too well. 
 
-If all goes well, I'd like an extended button press 
-(> 7 seconds) to  initiate a system shutdown.
+Also, if all goes well, an extended button press 
+(> 7 seconds) should initiate a Linux shutdown.
 
 ## Credits
 kalyaninagaraj
--- __init__.py
+++ __init__.py
@@ -1,44 +1,70 @@
-""" This skill follows the code outline of the excellent
-picroft-google-aiy-voicekit skill by @andlo, but uses
-the gpiozero library (instead of RPi.GPIO) to operate
-the button-led combo connected to the voicehat.
+""" This skill roughly follows the code outline of the
+excellent picroft-google-aiy-voicekit skill by @andlo,
+but uses the gpiozero library (instead of RPi.GPIO) to
+operate the button-led combo connected to the voicehat.
 
 The idea is to test gpiozero's ability to handle switch
-bounce when polling for a button press. RPi.GPIO does
-not fare too in these matters.
+bounce when polling for a button press; RPi.GPIO does
+not do too..
 
-If all goes well, I'd like an extended button press
-(> 7 seconds) to  initiate a system shutdown.
+Also, if all goes well, an extended button press
+(> 7 seconds) should initiate a Linux shutdown.
 """
 
-#from mycroft.messagebus.message import Message
 from mycroft import MycroftSkill
+from mycroft.messagebus.message import Message
 from gpiozero import Button, LED
-from signal import pause
 import time
-
-#button = Button(23)
-led = LED(25)
+import subprocess
 
 class GoogleAIYVoicehat(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
     def initialize(self):
-        led.off()
-        self.schedule_repeating_event(self.handle_voicehat_button, None, 0.01, 'buttonpress')
-        #self.add_event('recognizer_loop:record_begin', self.handle_listener_started)
-        #self.add_event('recognizer_loop:record_end', self.handle_listener_ended)
+        self.time_held = 100
+        self.short_press = 2
+        self.button = Button(23, hold_time = 7)
+        self.led = LED(25)
+        self.schedule_repeating_event(self.handle_button, None, 0.01, 'buttonpress')
 
-    def handle_voicehat_button(self, message):
-        if button.when_pressed
-            self.say_hello()
-        pause()
+    def handle_button(self):
+        self.button.when_pressed = self.start_timer
+        self.button.when_held = self.graceful_exit
+        self.button.when_released = self.stop_timer
+        self.add_event('recognizer_loop:record_begin', self.handle_listener_started)
+        self.add_event('recognizer_loop:record_end', self.handle_listener_ended)
 
-    def say_hello(self):
-        led.on()
-        self.speak("Hello!")
-        led.off()
+    def start_timer(self):
+        self.time_held = time.time()
+
+    def stop_timer(self):
+        self.time_held = time.time() - self.time_held
+        if self.time_held < self.short_press:
+            self.bus.emit(Message('mycroft.mic.listen'))
+        elif self.time_held < self.button.hold_time:
+            self.bus.emit(Message('mycroft.stop'))
+
+    def handle_listener_started(self):
+        self.led.on()
+
+    def handle_listener_ended(self):
+        self.led.off()
+
+    def graceful_exit(self):
+        self.blink()
+        self.log.info('Forcing a linux shutdown by the press of a button ...')
+        subprocess.call(['sudo', 'shutdown', '-h', 'now'])
+
+    def blink(self):
+        for i in range(6):
+            self.led.toggle()
+            time.sleep(0.5)
+
+    def shutdown(self):
+        self.cancel_scheduled_event('buttonpress')
+        self.button.close()
+        self.led.close()
 
 def create_skill():
     return GoogleAIYVoicehat()
